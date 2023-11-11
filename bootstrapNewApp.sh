b40e182490480a9e37735390a128fa8576154b14
Lucas van Staden <sales@proxiblue.com.au>
2023-11-11 03:26:01 +0000

    add in report folder

--- bootstrapNewApp.sh
+++ bootstrapNewApp.sh
@@ -7,6 +7,7 @@ mkdir data &&
 mkdir fixtures &&
 mkdir locators &&
 mkdir pages &&
+mkdir -p ./src/apps/$1/reports/allure/allure-result &&
 mkdir tests &&
 touch config.json &&
 touch package.json &&
@@ -14,5 +15,6 @@ touch playwright.config.ts &&
 touch data/home.data.json &&
 touch fixtures/index.ts &&
 printf "reports\ntest-results" > .gitignore
+
 
 
