bd844962fc6131b8a7e2701e827434be28fb7b47
Lucas van Staden <sales@proxiblue.com.au>
2024-10-22 13:00:31 +0800

    add in admin app as part of base repo

---
+++ src/apps/admin/locators/admin.locator.ts
@@ -0,0 +1,12 @@
+export const username = '#username';
+export const password = '#login';
+export const title = 'h1';
+export const logout_option = '.account-signout';
+export const admin_menu_sales = '#menu-magento-sales-sales';
+export const admin_menu_sales_orders = '.item-sales-order.level-2';
+export const admin_data_grid = '.admin__data-grid-wrap'
+export const admin_sales_order_grid_cells = '.data-grid-cell-content';
+export const action_menu_item = '.action-menu-item';
+export const admin_grid_search = '.data-grid-search-control';
+export const admin_grid_search_submit = '.data-grid-search-control-wrap .action-submit';
+export const remove_filter_button = '.admin__current-filters-list .action-remove';
---
+++ src/apps/admin/playwright.config.ts
@@ -0,0 +1,66 @@
+import {devices, PlaywrightTestConfig} from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+    testDir: process.env.TEST_BASE ? `../${process.env.TEST_BASE}/tests` : 'tests',
+    testMatch: "tests/*.spec.ts",
+    timeout: 30 * 1000,
+    retries: 1,
+    workers: 1,
+    globalSetup: require.resolve("@home/global-setup"),
+    globalTeardown: require.resolve("@home/global-teardown"),
+    expect: {
+        timeout: 20000,
+    },
+    use: {
+        headless: true,
+        actionTimeout: 0,
+        trace: "retain-on-failure",
+        ignoreHTTPSErrors: true,
+        video: "retain-on-failure",
+        screenshot: "only-on-failure",
+        acceptDownloads: true,
+        colorScheme: "dark",
+        launchOptions: {
+            slowMo: 500,
+        },
+    },
+    reporter: [
+        ["list"],
+        [
+            "json",
+            {
+                outputFile: "reports/json-reports/json-report.json",
+            },
+        ],
+        [
+            "html",
+            {
+                outputFolder: "reports/playwright-report/",
+                open: "never",
+            },
+        ],
+        [
+            "allure-playwright",
+            {
+                outputFolder: "reports/allure/allure-result/",
+                open: "never",
+            },
+        ],
+    ],
+    projects: [
+        {
+            name: "chromium",
+            use: {...devices["Desktop Chrome"] },
+        },
+        {
+            name: "firefox",
+            use: { ...devices["Desktop Firefox"] },
+        },
+        {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] },
+        }
+    ],
+};
+
+export default config;
