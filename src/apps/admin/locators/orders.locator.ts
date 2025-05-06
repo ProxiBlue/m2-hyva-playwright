a304e5bb99b9583a5bdbd3a5e95127721302bf0a
Lucas van Staden <sales@proxiblue.com.au>
2025-05-06 13:52:14 +0800

    add in orders page seperated from admin page

---
+++ src/apps/admin/locators/orders.locator.ts
@@ -0,0 +1,27 @@
+export const username = '#username';
+export const password = '#login';
+export const title = '.page-title';
+export const logout_option = '.account-signout';
+export const admin_menu_sales = '#menu-magento-sales-sales';
+export const admin_menu_sales_orders = '.item-sales-order.level-2';
+export const admin_data_grid = '.admin__data-grid-wrap'
+export const admin_sales_order_grid_cells = '.data-grid-cell-content';
+export const action_menu_item = '.action-menu-item';
+export const admin_grid_search = '.data-grid-search-control';
+export const admin_grid_search_submit = '.data-grid-search-control-wrap .action-submit';
+export const remove_filter_button = '.admin__current-filters-list .action-remove';
+export const create_new_order_button = '.page-actions-buttons #add'
+export const create_new_customer_button = 'Create New Customer'
+
+export const order_add_products = '#add_products'
+export const add_product_grid = '#sales_order_create_search_grid_table'
+export const add_product_to_order_button = '.actions .action-add'
+export const order_get_shipping_methods = 'text=Get shipping methods and rates';
+export const send_email_confirmation = '#send_confirmation';
+
+export const place_order_button = '.order-toatls-actions .actions .save.primary'
+
+export const admin_success_message = '.message.message-success.success'
+export const filter_button_expand = '[data-action="grid-filter-expand"]'
+export const filter_increment_id = '[name="increment_id"]'
+export const filter_apply = '[data-action="grid-filter-apply"]'
--- src/apps/admin/tests/checkout.spec.ts
+++ src/apps/admin/tests/checkout.spec.ts
@@ -1,11 +1,12 @@
 import {describe, test} from "@admin/fixtures";
+import * as locators from "@admin/locators/orders.locator";
 
 describe("Admin Checkout: Bank Transfer", () => {
 
-    test("checkout using bank transfer payment", async ({adminPage, customerData}, testInfo) => {
+    test("checkout using bank transfer payment", async ({adminPage, customerData, adminOrdersPage}, testInfo) => {
         // @ts-ignore
         test.skip(process.env.skipBaseTests.includes(testInfo.title), "Test skipped for this environment: " + process.env.APP_NAME);
-        await adminPage.navigateTo();
+        await ordersPage.navigateTo();
         await adminPage.login();
         await adminPage.navigateToOrdersPage();
         await adminPage.createNewOrderWithNewCustomer(customerData)
