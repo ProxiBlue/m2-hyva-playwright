892c16481680a967b352b64e6d33275280020503
Lucas van Staden <sales@proxiblue.com.au>
2025-06-12 21:58:13 +0800

    adjust so admin users one browser only. add docs to how locales work.

--- bootstrapNewApp.sh
+++ bootstrapNewApp.sh
@@ -9,7 +9,7 @@ fi
 APP="src/apps/$1"
 
 # Check if the directory exists
-if [ -d "$DIR" ]; then
+if [ -d "$APP" ]; then
   echo "Error: The app '$APP' already exists."
   exit 1
 fi
@@ -28,11 +28,5 @@ cp ../../../bootstrap_files/config.json ./ &&
 cp ../../../bootstrap_files/config.private.json ./ &&
 cp ../hyva/playwright.config.ts ./ &&
 cp ../../../bootstrap_files/package.json ./ &&
-cp ../../../bootstrap_files/run.sh ./ &&
 cp ../../../bootstrap_files/fixtures_index.ts ./fixtures/index.ts &&
 sed -i "s/NEW_APP_NAME/$1/g" package.json
-
-
-
-
-
